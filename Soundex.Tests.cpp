89d9ce90f007f5c8029b0a2a17fbe3c68b1787b7
Venugopal K <venu.shastri@gmail.com>
2024-09-13 11:38:40 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,1 +1,7 @@
+#include <gtest/gtest.h>
 
+TEST(SoundexTestSuite,generateSoundex_ReturnsEmptyStringForEmptyStringInput){
+    char soundex[5];
+    generateSoundex("", soundex);
+    EXPECT_EQ(soundex,"");
+}
